92d088830a91cd97e497a3dfb79fdcd6c7f1c022
pntwari <55207309+pntwari@users.noreply.github.com>
2020-04-30 17:47:52 -0400

    cleaning up

--- covid/App.js
+++ covid/App.js
@@ -208,25 +208,6 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#FFF",
   },
-  markerWrap: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  marker: {
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: "rgba(130,4,150, 0.9)",
-  },
-  ring: {
-    width: 24,
-    height: 24,
-    borderRadius: 12,
-    backgroundColor: "rgba(130,4,150, 0.3)",
-    position: "absolute",
-    borderWidth: 1,
-    borderColor: "rgba(130,4,150, 0.5)",
-  },
 });
 
 AppRegistry.registerComponent("mapfocus", () => msRona);
